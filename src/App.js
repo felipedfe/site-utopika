f10e9089d9db9edbd7301927e655b38e48306e00
Felipe Fernandes <felipedfe@gmail.com>
2022-12-06 14:06:56 -0300

    adiciona video e informacoes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
+import videoReel from "./assets/edit 1-2_OK.mp4"
+import logo from "./assets/LOGO UTOPIKA_.png"
 
 function App() {
   return (
-    <div className="App">
-        <h1>
-          UTOPIKA!
-        </h1>
-    </div>
+    <main className="App">
+      {/* <div className="iframe-container">
+          <iframe
+            src="https://player.vimeo.com/video/778199725?h=c1d00cc6da&autoplay=1&loop=1" 
+            title="reel"
+            frameBorder="0"
+            allow="autoplay; fullscreen; picture-in-picture"
+            allowFullScreen>
+          </iframe>
+        </div>
+        <script src="https://player.vimeo.com/api/player.js"></script> */}
+      <section className="reel-container">
+        <div className="transparency-layer" />
+        <div className="info-container">
+          <h1 className="info-title">Site em breve</h1>
+          <img className="info-logo" src={logo} alt="logo utopika"></img>
+          <h3>contato@utopika.com.br</h3>
+        </div>
+        <video className="reel" autoPlay muted loop>
+          <source type="video/mp4"
+            src={videoReel}
+          >
+          </source>
+        </video>
+      </section>
+    </main>
   );
 }
 
