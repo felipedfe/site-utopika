e5bb427d22231df342ff24d702123475fedd9b79
Felipe Fernandes <felipedfe@gmail.com>
2022-12-13 11:43:13 -0300

    muda a logo e link do instagram

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { FaVimeo } from 'react-icons/fa'
-import { FaInstagramSquare } from 'react-icons/fa'
 import { RiInstagramFill } from 'react-icons/ri'
 import './App.css';
-import logo from "./assets/LOGO UTOPIKA_.png"
+import logo from "./assets/LOGO UTOPIKA_OK.png"
 
 function App() {
   return (
@@ -17,9 +16,8 @@ function App() {
           <a href="https://vimeo.com/utopika" rel="noreferrer" target="_blank">
             <FaVimeo className="icon" />
           </a>
-          <a href="https://www.instagram.com/utopika_studio/" rel="noreferrer" target="_blank">
+          <a href="https://www.instagram.com/utopika_design/" rel="noreferrer" target="_blank">
             <RiInstagramFill className="icon" />
-          {/* <FaInstagramSquare className="icon" /> */}
           </a>
         </div>
       </div>
@@ -36,33 +34,6 @@ function App() {
           </iframe>
         </div>
       </div>
-
-      {/* <div 
-      className="iframe-container"
-      >
-        <iframe
-        title="video-reel"
-        src="https://player.vimeo.com/video/778199725?h=c1d00cc6da&autoplay=1&loop=1&title=0&byline=0&portrait=0&controls=0"
-        frameBorder="0"
-        >
-        </iframe>
-      </div>
-      <script src="https://player.vimeo.com/api/player.js"></script> */}
-
-      {/* <section className="reel-container">
-        <div className="transparency-layer" />
-        <div className="info-container">
-          <h1 className="info-title">Site em breve</h1>
-          <img className="info-logo" src={logo} alt="logo utopika"></img>
-          <h3>contato@utopika.com.br</h3>
-        </div>
-        <video className="reel" muted loop>
-          <source type="video/mp4"
-            src={videoReel}
-          >
-          </source>
-        </video>
-      </section> */}
     </main>
   );
 }
