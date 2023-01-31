06d6a2519455548722bd18cd79376179bc78b5d0
Felipe Fernandes <felipedfe@gmail.com>
2023-01-31 12:20:52 -0300

    feat: install react router dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Reel from './components/home-components/Reel/Reel';
 import Intro from './components/home-components/Intro/Intro';
 import Instagram from './components/home-components/Instagram/Instagram';
 import Menu from './components/Menu/Menu';
+import './App.css';
 
 function App() {
   return (
